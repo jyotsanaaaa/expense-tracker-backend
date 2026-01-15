db27b2e244716a6a2d9c6d0e882dbb7d74b38356
Jyotsana Priyadarsini <jyotsana.priyadarsini@gmail.com>
2026-01-15 21:55:32 +0530

    Fixed currency API using free Frankfurter API

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,7 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from .views import ExpenseViewSet
+from .views import ExpenseViewSet, convert_currency
+from django.urls import path
 
 router = DefaultRouter()
 router.register(r'expenses', ExpenseViewSet)
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path('convert/', convert_currency),
+]
--- expenses/views.py
+++ expenses/views.py
@@ -2,6 +2,24 @@ from rest_framework import viewsets
 from .models import Expense
 from .serializers import ExpenseSerializer
 
+import requests
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
 class ExpenseViewSet(viewsets.ModelViewSet):
     queryset = Expense.objects.all().order_by('-date')
     serializer_class = ExpenseSerializer
+
+
+@api_view(['GET'])
+def convert_currency(request):
+    amount = request.GET.get('amount')
+    from_currency = request.GET.get('from')
+    to_currency = request.GET.get('to')
+
+    url = f"https://api.frankfurter.app/latest?amount={amount}&from={from_currency}&to={to_currency}"
+    response = requests.get(url)
+    data = response.json()
+
+    return Response(data)
